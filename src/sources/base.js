526c6bc84fd7845fcc5209a1fe0092f03dd3d807
Jonas Gonzalez <alfredojonas94@gmail.com>
2023-07-14 14:38:18 -0400

    just sent a single message

--- src/sources/base.js
+++ src/sources/base.js
@@ -68,7 +68,7 @@ class BaseSource {
     const budgetSheet = SpreadsheetApp.openById(SETTINGS.getProperty('ssId')).getSheetByName('Presupuesto');
     budgetSheet.getRange('A3').setValue(category);
     const budgetDolares = parseFloat(budgetSheet.getRange('B3').getValue());
-    if (budgetDolares > 0 && budgetDolares < montoDolares) {
+    if (budgetDolares > 0 && montoDolares > budgetDolares) {
       return "WARNING: El monto ingresado supera el presupuesto de " + budgetDolares + "$";
     }
     return "";
@@ -103,7 +103,7 @@ class BaseSource {
         const expenseAdded = "GASTO: fecha=" + date + " | Categoria=" + item[0] + " | dolares=" + dolares + ", pesos=" + pesos + ", bolivares=" + bolivares;
         const exchangeRates = "Tasas de cambios: BS/USD=" + bsUsd.toFixed(2) + " | COP/BS=" + pesoBs.toFixed(2) + " | COP/USD=" + pesoUsd.toFixed(2);
         const budgetMessage = this.checkBudget(dolares, item[0]);
-        this.sendMessage(message + expenseAdded + exchangeRates + budgetMessage);
+        this.sendMessage(message);
       } else {
         throw new Error("ERROR: Verifique el formato del mensaje.");
       }
